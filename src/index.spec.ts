4a0f83af20b19b89663a544b8687aa435a12e673
Arhun Saday <arhunsad@gmail.com>
2023-06-22 17:49:09 +0200

    feat: add 2 tests

---
+++ src/index.spec.ts
@@ -0,0 +1,44 @@
+import { PopPlugin, PluginResponse } from '.';
+
+describe('PopPlugin', () => {
+  let popPlugin: PopPlugin;
+
+  beforeEach(() => {
+    popPlugin = new PopPlugin();
+  });
+
+  it('should return the plugin name', () => {
+    expect(popPlugin.name()).toBe('pop');
+  });
+
+  describe('run', () => {});
+
+  describe('respond_with', () => {
+    let mockStdout: jest.SpyInstance;
+
+    beforeEach(() => {
+      mockStdout = jest.spyOn(process.stdout, 'write');
+      mockStdout.mockImplementation(() => true);
+    });
+
+    afterEach(() => {
+      mockStdout.mockRestore();
+    });
+
+    it('should write the response to stdout', () => {
+      const response: PluginResponse = {
+        Append: {
+          id: 1,
+          name: 'test',
+          description: 'test description',
+          keywords: ['test'],
+          icon: { Name: 'test-icon' },
+          exec: 'test',
+          window: [100, 100],
+        },
+      };
+      popPlugin.respond_with(response);
+      expect(mockStdout).toHaveBeenCalledWith(`${JSON.stringify(response)}\n`);
+    });
+  });
+});
